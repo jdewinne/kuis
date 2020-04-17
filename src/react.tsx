178a3dd9cee2d6414b6dfbdb8efb0de82ce50d4d
Joris De Winne <joris.dewinne@gmail.com>
2020-04-17 10:51:05 -0700

    Adding eslint and codeclimate

--- src/react.tsx
+++ src/react.tsx
@@ -6,8 +6,14 @@ import k8s = require('@kubernetes/client-node');
 const kc = new k8s.KubeConfig();
 
 const Index = () => {
-    kc.loadFromDefault();
-    return <div>Hello {kc.currentContext}!</div>;
+  kc.loadFromDefault();
+  return (
+    <div>
+      Hello
+      {kc.currentContext}
+      !
+    </div>
+  );
 };
- 
+
 ReactDOM.render(<Index />, document.getElementById('app'));
