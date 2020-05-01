66613d97114fd0adf82cad17bb7a558eb671e9e7
Joris De Winne <joris.dewinne@gmail.com>
2020-05-01 16:50:41 -0700

    first dirty listing of all namespace names and the total number

--- src/react.tsx
+++ src/react.tsx
@@ -3,17 +3,41 @@ import * as ReactDOM from 'react-dom';
 
 import k8s = require('@kubernetes/client-node');
 
-const kc = new k8s.KubeConfig();
+class App extends React.Component<{}, any> {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0, namespaces: [] };
+  }
 
-const Index = () => {
-  kc.loadFromDefault();
-  return (
-    <div>
-      Hello
-      {kc.currentContext}
-      !
-    </div>
-  );
-};
+  componentDidMount() {
+    const kc = new k8s.KubeConfig();
+    kc.loadFromDefault();
+    const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+    k8sApi.listNamespace().then((res) => {
+      this.setState(() => ({ counter: res.body.items.length, namespaces: res.body.items }));
+    });
+  }
 
-ReactDOM.render(<Index />, document.getElementById('app'));
+  render() {
+    return (
+      <div>
+        Hello
+        microk8s
+        !
+        { ' '}
+        with namespaces:
+        {this.state.counter}
+        and
+        {this.state.namespaces.map((namespace) => {
+          console.log(namespace.metadata!.name);
+          // Return the element. Also pass key
+          return (namespace.metadata!.name);
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById('app'));
